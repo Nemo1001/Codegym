d5cf1d01299bf31a40bd705f7856100df8610f93
nemo1001 <phamhuyduc11094@gmail.com>
2019-07-05 15:29:15 +0700

    cap nhat bai tap

---
+++ Module1/week3/SS8/[Bài tập] Game đơn giản - di chuyển đối tượng/script.js
@@ -0,0 +1,34 @@
+/**
+ * Created by nhatnk on 4/26/17.
+ */
+
+function Hero(image, top, left, size){
+  this.image = image;
+  this.top = top;
+  this.left = left;
+  this.size = size;
+
+  this.getHeroElement = function(){
+    return '<img width="'+ this.size + '"' +
+      ' height="'+ this.size + '"' +
+      ' src="' + this.image +'"' +
+      ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
+  }
+
+  this.moveRight = function(){
+    this.left += 15;
+    console.log('ok: ' + this.left);
+  }
+}
+
+var hero = new Hero('pikachu.png', 10, 10, 80);
+
+function start(){
+  if(hero.left < window.innerWidth - hero.size){
+    hero.moveRight();
+  }
+  document.getElementById('game').innerHTML = hero.getHeroElement();
+  setTimeout(start, 500)
+}
+
+start();
---
+++ Module2/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        public class SystemTime {
+            public static void main(String[] args) {
+
+            }
+        }
+        import java.util.Date;
+
+        public class SystemTime {
+            public static void main(String[] args) {
+                Date now = new Date();
+
+                System.out.println("Now is: " + now);
+            }
+        }
+	// write your code here
+    }
+}
